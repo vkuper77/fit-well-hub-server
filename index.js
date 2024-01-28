12871ca7615cb7fe0b9cad39c58ac5879eea1362
vitali <vkuper7@gmail.com>
2024-01-28 21:38:47 +0300

    refactor: app

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const mongoose = require('mongoose');
 const router = require('./router/index')
 const errorMiddleware = require('./middlewares/error-middleware');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+app.use(cors({ credentials: true }));
 app.use('/api', router);
 app.use(errorMiddleware);
 
