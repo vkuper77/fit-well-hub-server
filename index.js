eba056c288ab925302b35b18b089cea69434333e
vitali <vkuper7@gmail.com>
2024-03-03 22:50:18 +0300

    refactor: app

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const router = require('./router/index')
-const errorMiddleware = require('./middlewares/error-middleware');
+const errorMiddleware = require('./middlewares/error-middleware')
 
-let URL_DB;
-const PORT = process.env.PORT || 3000;
+let URL_DB
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json());
@@ -29,10 +29,10 @@ const start = async () => {
         await mongoose.connect(URL_DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        })
+        }).then(() => console.log('Connected to MongoDB'))
         app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
     } catch (e) {
-        console.log(e);
+        console.error(e);
     }
 }
 
