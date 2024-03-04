1ebe7e3c89c9c71eb892303ae8d3eeed20d192f3
vitali <vkuper7@gmail.com>
2024-03-05 00:18:28 +0300

    refactor: type code

--- models/code-model.js
+++ models/code-model.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose');
 
 const CodeSchema = new Schema({
     email: { type: String, required: true },
-    code: { type: Number, required: true },
+    code: { type: String, required: true },
 })
 
 module.exports = model('Code', CodeSchema);
